6b7350491a87f4277bc9f53ffd44da07da36bf33
pesova <pesova13@gmail.com>
2020-12-26 21:56:27 +0100

    Finished wordpress course

--- wp-content/themes/bootstrap2wordpress/404.php
+++ wp-content/themes/bootstrap2wordpress/404.php
@@ -10,51 +10,79 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<section class="feature-image feature-image-default" data-type="background" data-speed="2">
+		<h1 class="page-title">Bummer! That page can&rsquo;t be found</h1>
+    </section>
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bootstrap-to-wordpress' ); ?></h1>
-			</header><!-- .page-header -->
+	<div class="container">
+		<div id="primary" class="row">
+			<main id="content" class="col-sm-8">
+				<div class="error-404 not-found">
+					<div class="page-content">
+						<h2>Don't fret Let's get you back on track</h2>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bootstrap-to-wordpress' ); ?></p>
+						<h3>Resources</h3>
+						<p>Perhaps you were looking for a specific resources</p>
 
-					<?php
-					get_search_form();
+						<?php $loop = new WP_Query( array( 'post_type' => 'resource', 'orderby' => 'post_id', 'order' => 'ASC') ); 
+                ?>
+						<div class="resource-row clearfix">
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bootstrap-to-wordpress' ); ?></h2>
-						<ul>
+						<?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+							
 							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
+								$resource_image	= get_field('resource_image');
+								$resource_url	= get_field('resource_url');
+								$button_text	= get_field('button_text');	
 							?>
-						</ul>
-					</div><!-- .widget -->
+							
+							<div class="resource">
 
-					<?php
-					/* translators: %1$s: smiley */
-					$bootstrap_to_wordpress_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bootstrap-to-wordpress' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$bootstrap_to_wordpress_archive_content" );
+								<img src="<?php echo $resource_image['url']; ?>" alt="<?php echo $resource_image['alt']; ?>">
+								
+								
+								<h3><a href="<?php echo $resource_url; ?>"><?php the_title(); ?></a></h3>
+								<?php the_excerpt(); ?>
+							
+								<?php if( !empty($button_text) ) : ?>
+								<a href="<?php echo $resource_url; ?>" class="btn btn-success"><?php echo $button_text; ?></a>
+								<?php endif; ?>
+							</div><!-- resource -->
+							
+						<?php endwhile; ?>
+						</div>
+						
+						<h3>Categories</h3>
+						<p>...or maybe a popular category?</p>
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+						<div class="widget widget_categories">
+							<h4 class="widget-title">Most Used Categories</h4>
+							<ul>
+								<?php wp_list_categories( array(
+									'orderby'	=> 'count',
+									'order'		=> 'DESC',
+									'title_li'	=> '',
+									'number'	=> '10'
+								)); ?>
+							</ul>
+						</div>
 
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+						<h3>Archives</h3>
+						<p>You can always sort through our archives...</p>
+						<?php the_widget( 'WP_Widget_Archives', 'title=Our Archives', 'before_title=<h4 class="widgettitle">&after_title=</h4>');?>
 
-	</main><!-- #main -->
+						  <p>...or, just head back to the <a href="<?php echo esc_url(home_url('/'))?>">home page</a> </p>
+					</div>
+				</div>
+			</main>
+
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>					
+			</aside>
+		</div>
+	</div>
+
+
 
 <?php
 get_footer();
--- wp-content/themes/bootstrap2wordpress/page-resources.php
+++ wp-content/themes/bootstrap2wordpress/page-resources.php
@@ -54,28 +54,7 @@ $thumbnial_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID) )
 							<?php endif; ?>
 						</div><!-- resource -->
 						
-						<?php endwhile; ?>
-                        
-
-
-                        <!-- <div class="resource">
-                            <img src="assets/img/coda2-logo.jpg" alt="Coda 2">
-                            <h3><a href="http://bradhussey.ca/coda2">Coda 2</a></h3>
-                            <p>If you&rsquo;re serious about coding, or you want to start out with a solid tool to code with, I strongly recommend Coda 2 for Mac. A beautiful User Interface with tabbed navigation, a super sidebar, and a simple layout. The
-                                features are outrageous and extensive, with code folding, smart complete, auto indentation, GIT integration, a built-in terminal, iCloud support &mdash; to name a few. The features seem to be endless.</p>
-                            <p>I use Coda 2 for all of my courses &amp; tutorials, and if you decide to use Coda 2 as your editor of choice, it would greatly benefit you when taking my courses. The price of the editor weighs in at $75, but it&rsquo;s worth
-                                every penny &mdash; in my honest opinion.</p>
-                            <a href="http://bradhussey.ca/coda2" class="btn btn-success">Check out Coda 2</a>
-                        </div>
-                        <div class="resource">
-                            <img src="assets/img/dropbox-logo.jpg" alt="Dropbox">
-                            <h3><a href="http://bradhussey.ca/getdropbox">Dropbox</a></h3>
-                            <p>This tool should go without saying. But if I have to explain &mdash; you <strong>need</strong> this tool. I use it 24/7 &mdash; literally. Dropbox is where I store my personal &amp; business related data. It's accessible anywhere,
-                                and regularly backs up my important information.</p>
-                            <p>Because my data is accessible anywhere, Dropbox allows me to work on a client project at home, on the bus, in a coffee shop, or at a friend's house. It's truly a gem of a tool, and I think you'll believe me.</p>
-                            <a href="http://bradhussey.ca/getdropbox" class="btn btn-success">Try Dropbox for free</a>
-                        </div> -->
-
+						<?php endwhile; wp_reset_query();?>
 
                     </div>
                 </section>
